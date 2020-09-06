eca4cb5146c22f2d56b57fc8f9407bf816b63b6f
thienphuit <thienphua7@gmail.com>
2020-09-06 11:33:09 +0700

    add navigation done

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
+import 'react-native-gesture-handler'
 import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 import { HomeScreen } from './src/screens'
+
+const Stack = createStackNavigator()
 
 const App = () => {
   return (
-    <HomeScreen />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 export default App
